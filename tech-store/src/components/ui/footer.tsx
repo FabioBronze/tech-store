550ade891eefd9a015399e7df5fe1556b68e5076
Fábio Bronze <fabio.r.bronze@hotmail.com>
2024-11-28 17:46:58 +0000

    feat: footer component

---
+++ tech-store/src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="mt-8 bg-accent p-5 py-4 text-[0.625rem] opacity-75">
+      &copy;2024 Copyright <span className="font-semibold">TechStore</span>
+    </footer>
+  );
+};
+
+export default Footer;
