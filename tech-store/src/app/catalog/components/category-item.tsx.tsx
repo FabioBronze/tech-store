8ebf613113329b6328d79386c5d1e37fafd6a012
Fábio Bronze <fabio.r.bronze@hotmail.com>
2024-11-29 17:17:26 +0000

    feat: Finish Catalog Component

---
+++ tech-store/src/app/catalog/components/category-item.tsx.tsx
@@ -0,0 +1,29 @@
+import { Category } from "@prisma/client";
+import Image from "next/image";
+
+interface CategoryItemProps {
+  category: Category;
+}
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  return (
+    <div className="flex flex-col">
+      <div className="rounded-tr-ls flex h-[150px] w-full items-center justify-center rounded-tl-lg bg-gradient-to-r from-[#5033C3] to-[rgba(80,51,195,0.20)]">
+        <Image
+          src={category.imageUrl}
+          alt={category.name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="max-w-auto h-auto max-h-[70%] w-auto"
+          style={{ objectFit: "contain" }}
+        />
+      </div>
+      <div className="rounded-bl-lg rounded-br-lg bg-accent p-2">
+        <p className="text-sm font-semibold">{category.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
--- tech-store/src/app/catalog/page.tsx
+++ tech-store/src/app/catalog/page.tsx
@@ -1,5 +1,28 @@
-const CatalogPage = () => {
-  return <h1>Catalog</h1>;
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item.tsx";
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany({});
+
+  return (
+    <div className="p-5 gap-8 flex flex-col mx-auto" style={{ maxWidth: "1200px" }}>
+      <Badge
+        className="gap-1 w-fit border-primary px-3 py-1 text-sm uppercase"
+        variant="outline"
+      >
+        <ShapesIcon size={16} />
+        Catalog
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {categories.map((category) => (
+          <CategoryItem category={category} key={category.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CatalogPage;
