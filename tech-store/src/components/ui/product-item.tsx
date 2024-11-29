928d33cb2b89c874ec303381abedaae723730adc
Fábio Bronze <fabio.r.bronze@hotmail.com>
2024-11-29 18:38:44 +0000

    fix: css adjustments on category and productIem component

--- tech-store/src/components/ui/product-item.tsx
+++ tech-store/src/components/ui/product-item.tsx
@@ -10,26 +10,24 @@ interface ProductItemProps {
 const ProductItem = ({ products }: ProductItemProps) => {
   return (
     <div className="flex flex-col gap-4 ">
-      <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
-        <Image
-          src={products.imageUrls[0]}
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="max-x-[80%] h-auto w-auto max-w-[70%]"
-          style={{
-            objectFit: "contain",
-          }}
-          alt={products.name}
-          priority
-        />
-        {products.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
-            <ArrowDownIcon size={14} />
-            {products.discountPercentage}%
-          </Badge>
-        )}
-      </div>
+      <div className="relative flex h-[180px] w-full items-center justify-center rounded-lg bg-accent">
+  <Image
+    src={products.imageUrls[0]}
+    height={170} // Defina um valor fixo para a altura
+    width={110}
+    sizes="100vw"
+    className="max-w-full max-h-full object-contain"
+    alt={products.name}
+    priority
+  />
+  {products.discountPercentage > 0 && (
+    <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+      <ArrowDownIcon size={14} />
+      {products.discountPercentage}%
+    </Badge>
+  )}
+</div>
+
       <div className="flex flex-col gap-1">
         <p className="test-sm w-full overflow-hidden text-ellipsis whitespace-nowrap">
           {products.name}
