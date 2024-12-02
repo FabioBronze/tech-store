92cf8fdf43d71d704e68cf7433351c264a3b4633
Fábio Bronze <fabio.r.bronze@hotmail.com>
2024-12-02 16:17:37 +0000

    feat: add CartItem component and improved cart product addition

---
+++ tech-store/src/components/ui/cart-item.tsx
@@ -0,0 +1,52 @@
+import { CartProduct } from "@/providers/cart";
+import Image from "next/image";
+import { Button } from "./button";
+import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from "lucide-react";
+
+interface CartItemProps {
+  product: CartProduct;
+}
+
+const CartItem = ({ product }: CartItemProps) => {
+  return (
+    <div className="flex items-center">
+      <div className="flex items-center">
+        <div className="flex h-[80px] w-[80px] items-center justify-center rounded-lg bg-accent">
+          <Image
+            src={product.imageUrls[0]}
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt={product.name}
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          />
+        </div>
+      </div>
+      <div className="ml-2 flex flex-col">
+        <p className="text-xs">{product.name}</p>
+        <div className="flex items-center gap-2">
+          <p className="text-sm font-bold">{product.totalPrice}€</p>
+          {product.discountPercentage > 0 && (
+            <p className="text-xs line-through opacity-75">
+              {Number(product.basePrice)}€
+            </p>
+          )}
+        </div>
+        <div className="mt-1 flex items-center gap-2">
+          <Button size="icon" variant="outline" className="h-6 w-6">
+            <ArrowLeftIcon size={16} />
+          </Button>
+          <span className="text-xs">{product.quantity}</span>
+          <Button size="icon" variant="outline" className="h-6 w-6">
+            <ArrowRightIcon size={16} />
+          </Button>
+        </div>
+      </div>
+      <Button size="icon" variant="outline" className="h-8 w-8 mx-auto">
+        <TrashIcon size={16}/>
+      </Button>
+    </div>
+  );
+};
+
+export default CartItem;
