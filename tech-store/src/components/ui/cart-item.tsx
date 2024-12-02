6acb683f71fa805e5b32af385eb4b92347073b5b
Fábio Bronze <fabio.r.bronze@hotmail.com>
2024-12-02 16:31:18 +0000

    feat: ensure product quantity can be modified on cart

--- tech-store/src/components/ui/cart-item.tsx
+++ tech-store/src/components/ui/cart-item.tsx
@@ -1,13 +1,20 @@
-import { CartProduct } from "@/providers/cart";
+import { CartContext, CartProduct } from "@/providers/cart";
 import Image from "next/image";
 import { Button } from "./button";
 import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from "lucide-react";
+import { useContext } from "react";
 
 interface CartItemProps {
   product: CartProduct;
 }
 
 const CartItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity } = useContext(CartContext);
+
+  const handleDecreaseProductQuantityClick = () => {
+    decreaseProductQuantity(product.id);
+  };
+
   return (
     <div className="flex items-center">
       <div className="flex items-center">
@@ -33,7 +40,12 @@ const CartItem = ({ product }: CartItemProps) => {
           )}
         </div>
         <div className="mt-1 flex items-center gap-2">
-          <Button size="icon" variant="outline" className="h-6 w-6">
+          <Button
+            size="icon"
+            variant="outline"
+            className="h-6 w-6"
+            onClick={handleDecreaseProductQuantityClick}
+          >
             <ArrowLeftIcon size={16} />
           </Button>
           <span className="text-xs">{product.quantity}</span>
@@ -42,8 +54,8 @@ const CartItem = ({ product }: CartItemProps) => {
           </Button>
         </div>
       </div>
-      <Button size="icon" variant="outline" className="h-8 w-8 mx-auto">
-        <TrashIcon size={16}/>
+      <Button size="icon" variant="outline" className="mx-auto h-8 w-8">
+        <TrashIcon size={16} />
       </Button>
     </div>
   );
