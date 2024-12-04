fed08b3314991256142b94a0619377b75ee37331
Fábio Bronze <fabio.r.bronze@hotmail.com>
2024-12-04 20:22:10 +0000

    Update prisma.ts

--- tech-store/src/lib/prisma.ts
+++ tech-store/src/lib/prisma.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // Declare global.cachedPrisma as a PrismaClient instance
-  var cachedPrisma: PrismaClient | undefined;
+    // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
 }
 
 let prisma: PrismaClient;
